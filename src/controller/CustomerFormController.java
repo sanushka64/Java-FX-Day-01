83ccea3905a7f341626d4f88b2e80105d3a296cd
sanushka64 <sanushkadias@gmail.com>
2025-05-04 15:20:26 +0530

    add customer form

--- src/controller/CustomerFormController.java
+++ src/controller/CustomerFormController.java
@@ -1,7 +1,17 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Customer;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Observable;
 
 public class  CustomerFormController {
     public TextField txtId;
@@ -10,8 +20,31 @@ public class  CustomerFormController {
     public TextField txtName;
     public TextField txtSalary;
     public TextField txtEmail;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colPhone;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+    public TableView tblCustomers;
+    ArrayList<Customer> customerList = new ArrayList<>();
 
     public void btnAddCustomerOnAction(ActionEvent actionEvent) {
-        System.out.println("click");
+        customerList.add(new Customer(Integer.parseInt(txtId.getText()),txtName.getText(),txtEmail.getText(),txtAddress.getText(),txtPhone.getText(),Double.parseDouble(txtSalary.getText())));
+        btnReloadOnAction(actionEvent);
+    }
+
+    public void btnReloadOnAction(ActionEvent actionEvent) {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colPhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        ObservableList<Customer> customerObservableList = FXCollections.observableArrayList();
+        customerList.forEach(customer -> {
+            customerObservableList.add(customer);
+        });
+        tblCustomers.setItems(customerObservableList);
     }
 }
--- src/model/Customer.java
+++ src/model/Customer.java
@@ -4,7 +4,80 @@ public class Customer {
     private Integer id;
     private String name;
     private String email;
-    private String addrerss;
+    private String address;
     private String phone;
     private Double salary;
+
+    public Customer(Integer id, String name, String email, String address, String phone, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+        this.salary = salary;
+    }
+
+    public Customer() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
+
